ed655fb866311defabc8423a45b192ddcab727be
zleevan <422234924@qq.com>
2016-04-18 11:16:53 +0800

    Update config.php
    change mysql

--- App/Common/Conf/config.php
+++ App/Common/Conf/config.php
@@ -12,10 +12,10 @@ return array(
 	'URL' =>'http://www.example.com', //网站根URL
 	//数据库链接配置
 	'DB_TYPE'   => 'mysql', // 数据库类型
-	'DB_HOST'   => 'localhost', // 服务器地址
+	'DB_HOST'   => '10.66.116.79', // 服务器地址
 	'DB_NAME'   => 'industry', // 数据库名
 	'DB_USER'   => 'root', // 用户名
-	'DB_PWD'    => 'zhangzhen', // 密码
+	'DB_PWD'    => 'zhang123', // 密码
 	'DB_PORT'   => 3306, // 端口
 	'DB_PREFIX' => 'qw_', // 数据库表前缀
 	'DB_CHARSET'=>  'utf8',      // 数据库编码默认采用utf8
